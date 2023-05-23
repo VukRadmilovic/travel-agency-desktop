6d1dcaf7a4b03635b7dbefcd94bf456aba859f60
dracooya <maya.grudge.12@gmail.com>
2023-05-24 01:11:51 +0200

    Added initial project. Configured database connection with entity framework and added dummy User class for test.

---
+++ Turisticka_Agencija/Turisticka_Agencija/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turisticka_Agencija
+{
+    internal class Context : DbContext
+    {
+        public Context() : base("Data Source=(localdb)\\ProjectModels;Initial Catalog=AgencijaDb;Integrated Security=True") { }
+        public DbSet<User> Users { get; set; }  
+    }
+}
---
+++ Turisticka_Agencija/Turisticka_Agencija/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Turisticka_Agencija
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //ODRADI SAMO PRVI PUT DA VIDIS DA LI TI RADI KAKO TREBA ENTITY!
+           /* using (var ctx = new Context())
+            {
+                var user = new User("Maja", "Varga", "maja.varga@mail.com", "lozinka321");
+                ctx.Users.Add(user);
+                ctx.SaveChanges();
+            }*/
+        }
+    }
+}

ba2bc47ae52188e1ab0fa230f173a6241481ac21
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2023-06-12 15:37:25 +0200

    Make trip CRUD functional

--- Turisticka_Agencija/Turisticka_Agencija/Windows/Admin/CRUDTripWindow.xaml.cs
+++ Turisticka_Agencija/Turisticka_Agencija/Windows/Admin/CRUDTripWindow.xaml.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media;
+using MaterialDesignThemes.Wpf;
+using Microsoft.Maps.MapControl.WPF;
+using Turisticka_Agencija.Models;
+using Turisticka_Agencija.Services;
 using Turisticka_Agencija.Windows.Shared;
 
 namespace Turisticka_Agencija.Windows.Admin
@@ -26,6 +33,10 @@ namespace Turisticka_Agencija.Windows.Admin
         public static RoutedCommand ModifyCommand { get; } = new();
         public static RoutedCommand DeleteCommand { get; } = new();
 
+        private ObservableCollection<Place> _currentPlaces = new();
+        private ObservableCollection<Restaurant> _currentRestaurants = new();
+        private ObservableCollection<Accommodation> _currentAccommodations = new();
+
         public CRUDTripWindow()
         {
             InitializeComponent();
@@ -40,6 +51,85 @@ namespace Turisticka_Agencija.Windows.Admin
             ClearFieldsCommand.InputGestures.Add(new KeyGesture(Key.O, ModifierKeys.Control));
 
             Loaded += (sender, e) => FocusManager.SetFocusedElement(this, TripsTable);
+
+            TripsTable.ItemsSource = TripService.GetAll();
+            AvailablePlacesTable.ItemsSource = PlaceService.GetAll();
+            AvailableRestaurantsTable.ItemsSource = RestaurantService.GetAll();
+            AvailableAccommodationsTable.ItemsSource = AccommodationService.GetAll();
+        }
+
+        private void TripsTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedTrip = (Trip)TripsTable.SelectedItem;
+
+            if (selectedTrip == null)
+            {
+                DeleteButton.IsEnabled = false;
+                ModifyButton.IsEnabled = false;
+                return;
+            }
+
+            DeleteButton.IsEnabled = true;
+            ModifyButton.IsEnabled = true;
+
+            NameTextBox.Text = selectedTrip.Name;
+            PriceTextBox.Text = selectedTrip.Price.ToString();
+            FromDate.Text = selectedTrip.Start.ToShortDateString();
+            ToDate.Text = selectedTrip.End.ToShortDateString();
+            SoldTextBox.Text = TripBoughtOrReservedByUserService.NumberOfSold(selectedTrip).ToString();
+            DescriptionTextBox.Text = selectedTrip.Description;
+
+            _currentPlaces = new ObservableCollection<Place>(selectedTrip.Places);
+            _currentRestaurants = new ObservableCollection<Restaurant>(selectedTrip.Restaurants);
+            _currentAccommodations = new ObservableCollection<Accommodation>(selectedTrip.Accommodations);
+            
+            ThisTripPlacesTable.ItemsSource = _currentPlaces;
+            ThisTripRestaurantsTable.ItemsSource = _currentRestaurants;
+            ThisTripAccommodationsTable.ItemsSource = _currentAccommodations;
+
+            Map.Children.Clear();
+
+            if (selectedTrip.Accommodations != null)
+            {
+                foreach (Accommodation accommodation in selectedTrip.Accommodations)
+                {
+                    var packIcon = new PackIcon();
+                    packIcon.Kind = PackIconKind.Home;
+                    AddPin(accommodation.Latitude, accommodation.Longitude, accommodation.Name, Brushes.IndianRed, packIcon);
+                }
+            }
+
+            if (selectedTrip.Restaurants != null)
+            {
+                foreach (Restaurant restaurant in selectedTrip.Restaurants)
+                {
+                    var packIcon = new PackIcon();
+                    packIcon.Kind = PackIconKind.Restaurant;
+                    AddPin(restaurant.Latitude, restaurant.Longitude, restaurant.Name, Brushes.DarkCyan, packIcon);
+                }
+            }
+
+            if (selectedTrip.Places != null)
+            {
+                foreach (Place place in selectedTrip.Places)
+                {
+                    var packIcon = new PackIcon();
+                    packIcon.Kind = PackIconKind.Landscape;
+                    AddPin(place.Latitude, place.Longitude, place.Name, Brushes.DarkOrange, packIcon);
+                }
+            }
+        }
+
+        private void AddPin(double latitude, double longitude, string heading, SolidColorBrush color, PackIcon icon)
+        {
+            var location = new Location(latitude, longitude);
+            var pin = new Pushpin();
+            pin.Location = location;
+            pin.ToolTip = heading;
+            pin.Content = icon;
+            pin.Background = color;
+            Map.Children.Add(pin);
+            Map.Center = location;
         }
 
         private void RestaurantCRUD_OnClick(object sender, RoutedEventArgs e)
@@ -70,11 +160,6 @@ namespace Turisticka_Agencija.Windows.Admin
             Close();
         }
 
-        private void TripsTable_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
         private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (!Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl)) return;
@@ -96,22 +181,217 @@ namespace Turisticka_Agencija.Windows.Admin
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Cleared");
+            if (!ClearButton.IsEnabled)
+            {
+                return;
+            }
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            NameTextBox.Clear();
+            PriceTextBox.Clear();
+            FromDate.Clear();
+            ToDate.Clear();
+            SoldTextBox.Clear();
+            DescriptionTextBox.Clear();
+
+            _currentPlaces.Clear();
+            _currentRestaurants.Clear();
+            _currentAccommodations.Clear();
+
+            Map.Children.Clear();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Saved");
+            if (!SaveButton.IsEnabled)
+            {
+                return;
+            }
+
+            if (NameTextBox.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Morate uneti naziv putovanja.");
+                return;
+            }
+
+            DateTime from;
+            DateTime to;
+            double price;
+
+            string fromDate = FromDate.Text;
+            string toDate = ToDate.Text;
+
+            if (fromDate.EndsWith("."))
+            {
+                fromDate = fromDate.Substring(0, fromDate.Length - 1);
+            }
+
+            if (toDate.EndsWith("."))
+            {
+                toDate = toDate.Substring(0, toDate.Length - 1);
+            }
+
+            bool fromParsed = DateTime.TryParseExact(fromDate, "d.M.yyyy", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out from);
+            bool toParsed = DateTime.TryParseExact(toDate, "d.M.yyyy", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out to);
+
+            if (!fromParsed || !toParsed)
+            {
+                MessageBox.Show("Morate uneti datume u formatu dan.mesec.godina (npr. 23.5.2022)");
+                return;
+            }
+
+            if (from > to)
+            {
+                MessageBox.Show("Datum polaska je posle datuma dolaska. Molim Vas proverite datume ponovo.");
+                return;
+            }
+
+            bool priceParsed = double.TryParse(PriceTextBox.Text, out price);
+            if (!priceParsed)
+            {
+                MessageBox.Show("Morate uneti cenu kao decimalan broj.");
+                return;
+            }
+
+            Trip trip = new Trip(0, NameTextBox.Text, from, to, price, DescriptionTextBox.Text,
+                _currentPlaces, _currentAccommodations, _currentRestaurants);
+            bool success = TripService.Save(trip);
+            if (!success)
+            {
+                MessageBox.Show("Došlo je do greške prilikom upisa podataka. Molim Vas pokušajte ponovo uskoro.");
+                return;
+            }
+
+            ClearFields();
+            NameTextBox.Focus();
+            TripsTable.ItemsSource = TripService.GetAll();
+            if (SuccessSnackbar.MessageQueue is { } messageQueue)
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Putovanje uspešno dodato."));
         }
 
         private void ModifyButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Modified");
+            if (!ModifyButton.IsEnabled)
+            {
+                return;
+            }
+
+            var selectedTrip = (Trip)TripsTable.SelectedItem;
+
+            if (selectedTrip == null)
+            {
+                MessageBox.Show("Morate odabrati koje putovanje želite da izmenite iz tabele.");
+                return;
+            }
+
+            if (NameTextBox.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Morate uneti naziv putovanja.");
+                return;
+            }
+
+            DateTime from;
+            DateTime to;
+            double price;
+
+            string fromDate = FromDate.Text;
+            string toDate = ToDate.Text;
+
+            if (fromDate.EndsWith("."))
+            {
+                fromDate = fromDate.Substring(0, fromDate.Length - 1);
+            }
+
+            if (toDate.EndsWith("."))
+            {
+                toDate = toDate.Substring(0, toDate.Length - 1);
+            }
+
+            bool fromParsed = DateTime.TryParseExact(fromDate, "d.M.yyyy", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out from);
+            bool toParsed = DateTime.TryParseExact(toDate, "d.M.yyyy", CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out to);
+
+            if (!fromParsed || !toParsed)
+            {
+                MessageBox.Show("Morate uneti datume u formatu dan.mesec.godina (npr. 23.5.2022)");
+                return;
+            }
+
+            if (from > to)
+            {
+                MessageBox.Show("Datum polaska je posle datuma dolaska. Molim Vas proverite datume ponovo.");
+                return;
+            }
+
+            bool priceParsed = double.TryParse(PriceTextBox.Text, out price);
+            if (!priceParsed)
+            {
+                MessageBox.Show("Morate uneti cenu kao decimalan broj.");
+                return;
+            }
+
+            selectedTrip.Name = NameTextBox.Text;
+            selectedTrip.Start = from;
+            selectedTrip.End = to;
+            selectedTrip.Price = price;
+            selectedTrip.Description = DescriptionTextBox.Text;
+            selectedTrip.Places = _currentPlaces;
+            selectedTrip.Accommodations = _currentAccommodations;
+            selectedTrip.Restaurants = _currentRestaurants;
+
+            bool success = TripService.Update(selectedTrip);
+            if (!success)
+            {
+                MessageBox.Show("Došlo je do greške prilikom izmene podataka. Molim Vas pokušajte ponovo uskoro.");
+                return;
+            }
+
+            ClearFields();
+            TripsTable.ItemsSource = TripService.GetAll();
+            if (SuccessSnackbar.MessageQueue is { } messageQueue)
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Putovanje uspešno izmenjeno."));
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Deleted");
+            if (!DeleteButton.IsEnabled)
+            {
+                return;
+            }
+
+            var selectedTrip = (Trip)TripsTable.SelectedItem;
+
+            if (selectedTrip == null)
+            {
+                MessageBox.Show("Morate odabrati koje putovanje želite da obrišete iz tabele.");
+                return;
+            }
+
+            var decision = MessageBox.Show("Da li ste sigurni da želite da trajno obrišete ovo putovanje?",
+                "Potvrda brisanja", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (decision == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            bool success = TripService.Delete(selectedTrip);
+            if (!success)
+            {
+                MessageBox.Show("Došlo je do greške prilikom brisanja podataka. Molim Vas pokušajte ponovo uskoro.");
+                return;
+            }
+
+            ClearFields();
+            TripsTable.ItemsSource = TripService.GetAll();
+            if (SuccessSnackbar.MessageQueue is { } messageQueue)
+                Task.Factory.StartNew(() => messageQueue.Enqueue("Putovanje uspešno obrisano."));
         }
     }
 }

3345afee734a34c898d5ab40bc5e9c69102b60e3
Marko Milijanovic <markomilijanovic01@gmail.com>
2023-06-12 13:28:25 +0200

    Help folder structure and Util

---
+++ Turisticka_Agencija/Turisticka_Agencija/Migrations/202306120840130_MigratnM.cs
@@ -0,0 +1,16 @@
+﻿namespace Turisticka_Agencija.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigratnM : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
